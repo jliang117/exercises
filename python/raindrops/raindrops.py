d08c478e79137336b31e0da6f6dd07d4cbb9495b
jliang117 <jliang117@users.noreply.github.com>
2019-03-30 14:09:44 -0400

    raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,24 @@
 def raindrops(number):
-    pass
+    ret = str(number)
+    rainStr = ""
+    rainStr += stringIfNumModuloBy(number,3,'Pling')
+    rainStr += stringIfNumModuloBy(number,5,'Plang')
+    rainStr += stringIfNumModuloBy(number,7,'Plong')
+
+    return ret if rainStr=="" else rainStr
+
+def stringIfNumModuloBy(num,mod,string):
+	if (num % mod)==0:
+		return string
+	else: return ''
+
+print(raindrops(105))
+"""
+assumptions
+all inputs are integers
+how to handle intersections?
+3,5->15
+5,7->35
+3,7->21
+does order matter?
+"""
