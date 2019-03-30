af67aa0d5cc6984b337d7c862bd4746d99269263
jliang117 <jliang117@users.noreply.github.com>
2019-03-30 16:27:00 -0400

    done word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,7 +1,40 @@
+from collections import Counter
 def word_count(phrase):
-    from collections import Counter
-    c = Counter(phrase.split(" "))
-    phraseDict = dict(c)
-    return phraseDict
+	phrase = normalize(phrase)
+	pList = phrase.split()
+	pList = removeQuotes(pList)
+	c = Counter(pList)
+	phraseDict = dict(c)
+	return phraseDict
+
+def removeQuotes(phraseList):
+	return [word.strip('\'') for word in phraseList]
+
+def normalize(phrase):
+	phrase = normalizeDelim(phrase)
+	phrase = normalizeCase(phrase)
+	phrase = removePunc(phrase)
+	return phrase
+
+def normalizeDelim(phrase):
+	delimSet = {'\n',' ',':',',','_'}
+	for ele in delimSet:
+		phrase = phrase.replace(ele," ")
+	return phrase
 
 
+
+def removePunc(phrase):
+	puncSet = {'.','!','&','@','$','%','^','&'}
+	for ele in puncSet:
+		phrase = phrase.replace(ele,"")
+	return phrase
+
+def normalizeCase(phrase):
+	return phrase.lower()
+
+
+phrase = "Joe can't tell between 'large' and large."
+# print(normalize(phrase))
+
+print(word_count(phrase))
